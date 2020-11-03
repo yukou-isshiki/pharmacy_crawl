504689f3f7bc4679ef7315ca2dd8c07b1ba1e9b6
yukou-isshiki <yff24517@nifty.com>
2020-11-03 22:38:17 +0900

    解析結果追加

--- pharmacy_analysis.py
+++ pharmacy_analysis.py
@@ -14,18 +14,40 @@ print(df.sort_values(1, ascending=False).head(20))
 
 print(df.sort_values(1, ascending=False).tail(20))
 
+img1 = plt.figure()
+
 df[1].hist(bins=25)
 
 plt.show()
 
+img1.savefig("img1.png")
+
 low_limit = df[df[1] <= 3000]
+
+img2 = plt.figure()
 
 low_limit[1].hist()
 
 plt.show()
 
+img2.savefig("img2.png")
+
 high_limit = df[df[1] > 3000]
+
+img3 = plt.figure()
 
 high_limit[1].hist()
 
 plt.show()
+
+img3.savefig("img3.png")
+
+very_low_limit = df[df[1] < 100]
+
+img4 = plt.figure()
+
+very_low_limit[1].hist()
+
+plt.show()
+
+img4.savefig("img4.png")
